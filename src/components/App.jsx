5b076bb477eafaaf0f5f27583191473e26c4c7bf
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-16 10:13:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,52 @@
 import { Component } from 'react';
-import Modal from './Modal/Modal';
+import { Box } from './Box';
+import { Searchbar } from './Searchbar/Searchbar';
+// import Modal from './xModal/xModal';
 
 export class App extends Component {
-  state = {
-    showModal: false,
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
+  state = {};
 
   render() {
-    const { showModal } = this.state;
-
     return (
-      <>
-        <button type="button" onClick={this.toggleModal}>
-          Open Modal
-        </button>
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <h1>Title</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
-              ducimus!
-            </p>
-            <button type="button" onClick={this.toggleModal}>Close Modal</button>
-          </Modal>
-        )}
-      </>
+      <Box display="grid" gridTemplateColumns="1fr" gridGap={4} pb={5}>
+        <Searchbar />
+      </Box>
     );
   }
 }
+
+// export class App extends Component {
+//   state = {
+//     showModal: false,
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//     }));
+//   };
+
+//   render() {
+//     const { showModal } = this.state;
+
+//     return (
+//       <>
+//         <button type="button" onClick={this.toggleModal}>
+//           Open Modal
+//         </button>
+//         {showModal && (
+//           <Modal onClose={this.toggleModal}>
+//             <h1>Title</h1>
+//             <p>
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+//               ducimus!
+//             </p>
+//             <button type="button" onClick={this.toggleModal}>
+//               Close Modal
+//             </button>
+//           </Modal>
+//         )}
+//       </>
+//     );
+//   }
+// }
