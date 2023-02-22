6b34e8e9e7fab4bfdd612f3d85ea58e4894e5112
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-22 16:08:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'index.styled';
+import { GlobalStyle } from 'GlobalStyle';
 import { theme } from 'theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
