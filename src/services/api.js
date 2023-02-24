5ed03dc5204fac7ae35122527ab0aff14788c905
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-24 09:51:13 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,26 +2,16 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const fetchImage = async (q='') => {
-
-// console.log(q);
-
+export const fetchImage = async (q = '', page = 1) => {
   const response = await axios.get('', {
     params: {
       key: '31041219-f816f941255d17bdaf0e3f2df',
       image_type: 'photo',
       orientation: 'horizontal',
       q,
-      //   page: 1,
+      page,
       per_page: 12,
     },
   });
   return response.data;
 };
-
-// export const fetchImage = async () => {
-//   const response = await axios.get(
-//     '?key=31041219-f816f941255d17bdaf0e3f2df&q=cat&image_type=photo'
-//   );
-//   return response.data;
-// };
