14be915d614f8fdb61a8f7d4ed1875997d8e8e99
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-24 09:51:51 +0200

    more downloads images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import { Box } from './Box';
 import { Searchbar } from './Searchbar/Searchbar';
-import { Modal } from './Modal/Modal';
 import { fetchImage } from 'services/api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
@@ -15,35 +14,72 @@ const Status = {
 
 export class App extends Component {
   state = {
+    request: '',
     images: [],
     error: null,
     status: Status.IDLE,
-    showModal: false,
+    page: 1,
+    totalPages: 1,
   };
 
-  handleFormSubmit = imageName => {
-    this.getSearchImage(imageName);
+  componentDidUpdate(_, prevState) {
+    const { request, page } = this.state;
+    const prevRequest = prevState.request;
+    const prevPage = prevState.page;
+    const nextRequest = request;
+    const nextPage = page;
+
+    if (prevRequest !== nextRequest) {
+      this.setState({ images: [], page: 1 });
+    }
+
+    if (prevRequest !== nextRequest || prevPage !== nextPage) {
+      this.getSearchImage();
+    }
+  }
+
+  handleFormSubmit = request => {
+    this.setState({ request });
   };
 
-  getSearchImage = async name => {
+  getSearchImage = async () => {
+    const { request, page } = this.state;
+
     try {
       this.setState({ status: Status.PENDING });
-      const images = await fetchImage(name);
-      this.setState({ images: images.hits, status: Status.RESOLVED });
+      const images = await fetchImage(request, page);
+      // console.log(images);
+      // console.log(images.total);
+      // console.log(typeof images.total);
+
+      if (images.total <= 12) {
+        // console.log('length <= 12');
+        this.setState({
+          images: images.hits,
+          status: Status.RESOLVED,
+        });
+      } else {
+        // console.log('length > 12');
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images.hits],
+          totalPages: Math.ceil(images.total / 12),
+          status: Status.RESOLVED,
+        }));
+      }
     } catch (error) {
       this.setState({ error, status: Status.REJECTED });
       console.log(error.message);
     }
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
+  onClickLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
     }));
   };
 
   render() {
-    const { showModal, images, status } = this.state;
+    const { images, status, page, totalPages } = this.state;
 
     return (
       <Box display="grid" gridTemplateColumns="1fr" gridGap={4} pb={5}>
@@ -51,12 +87,14 @@ export class App extends Component {
 
         {status === Status.PENDING && <Loader />}
 
-        {images.length > 0 && <ImageGallery photos={images} />}
+        {status === Status.RESOLVED && images.length > 0 && (
+          <ImageGallery photos={images} />
+        )}
 
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <Box>modal</Box>
-          </Modal>
+        {page < totalPages && (
+          <button type="button" onClick={this.onClickLoadMore}>
+            Load more
+          </button>
         )}
       </Box>
     );
